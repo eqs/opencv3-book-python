b0a9e571f10c538f4709ca23a410e88b228d4473
eqs <i11murasige@gmail.com>
2018-10-20 19:40:55 +0900

    add image files

---
+++ images/get_images.sh
@@ -0,0 +1,5 @@
+wget http://www.ess.ic.kanagawa-it.ac.jp/std_img/colorimage/color.zip
+wget http://www.ess.ic.kanagawa-it.ac.jp/std_img/monoimage/mono.zip
+unzip color.zip -d color && rm color.zip
+unzip mono.zip -d mono && rm mono.zip
+
